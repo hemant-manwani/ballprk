45753268172a28062d42d15b98799cd1ccc7aa69
Hemant <hemantmanwani.iitr@gmail.com>
2018-12-19 19:25:40 +0530

    Add 4 more products

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -44,100 +44,30 @@ export const productList = [
   {
     id: 7,
     currency: '$',
-    price: '55.00',
-    name: 'Women T-shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt.jpeg'
+    price: '199.00',
+    name: "Women's T shirt",
+    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/women-t-shirt-101.jpeg'
   },
   {
     id: 8,
     currency: '$',
-    price: '299.00',
-    name: 'Men shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/mens-shirt.jpeg'
+    price: '249.00',
+    name: "Women's T shirt",
+    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/women-t-shirt-102.jpeg'
   },
   {
     id: 9,
     currency: '$',
-    price: '599.00',
-    name: 'Men Jacket',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/mens-jacket.jpeg'
+    price: '299.00',
+    name: "Women's T shirt",
+    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt-103.jpeg'
   },
   {
     id: 10,
     currency: '$',
-    price: '59.99',
-    name: 'Women T-shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt-2.jpeg'
-  },
-  {
-    id: 11,
-    currency: '$',
-    price: '99.99',
-    name: 'Girl Top',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/girsl-shirt.jpeg'
-  },
-  {
-    id: 12,
-    currency: '$',
-    price: '85.00',
-    name: 'Kids Clothing',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/kids-wear.jpeg'
-  },
-  {
-    id: 13,
-    currency: '$',
-    price: '85.00',
-    name: 'Kids Clothing',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/kids-wear.jpeg'
-  },
-  {
-    id: 14,
-    currency: '$',
-    price: '55.00',
-    name: 'Women T-shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt.jpeg'
-  },
-  {
-    id: 15,
-    currency: '$',
-    price: '299.00',
-    name: 'Men shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/mens-shirt.jpeg'
-  },
-  {
-    id: 16,
-    currency: '$',
-    price: '599.00',
-    name: 'Men Jacket',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/mens-jacket.jpeg'
-  },
-  {
-    id: 17,
-    currency: '$',
-    price: '59.99',
-    name: 'Women T-shirt',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt-2.jpeg'
-  },
-  {
-    id: 18,
-    currency: '$',
-    price: '99.99',
-    name: 'Girl Top',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/girsl-shirt.jpeg'
-  },
-  {
-    id: 19,
-    currency: '$',
-    price: '85.00',
-    name: 'Kids Clothing',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/kids-wear.jpeg'
-  },
-  {
-    id: 20,
-    currency: '$',
-    price: '85.00',
-    name: 'Kids Clothing',
-    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/kids-wear.jpeg'
+    price: '249.00',
+    name: "Women's T shirt",
+    image: 'https://s3.ap-south-1.amazonaws.com/e-notice/womens-t-shirt-104.jpeg'
   }
 ]
 
